627fe7d822cef864a78dc9725b9a623731ca1264
Tyler Allen <tylerthomasallen@gmail.com>
2020-06-19 13:39:35 -0700

    add history

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+// Components
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Home />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +22,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const Home = () => {
+
+  return (
+    <div>
+      <h1>Juneteenth</h1>
+      <h3>
+        History of Juneteenth (
+        <a href="https://en.wikipedia.org/wiki/Juneteenth">Wikipedia</a>)
+      </h3>
+      <div>
+        <span>
+          Juneteenth is a non-federal American holiday and an
+          official Texas state holiday, celebrated annually on the 19th
+          of June in the United States to commemorate Union army general Gordon
+          Granger announcing federal orders in the city of Galveston, Texas, on
+          June 19, 1865, proclaiming that all slaves in Texas were now free. 
+        </span>
+      </div>
+
+      <br />
+
+      <div>
+        <span>
+          Although the Emancipation Proclamation had formally freed them almost
+          two and a half years earlier and the American Civil War had largely
+          ended with the defeat of the Confederate States in April, Texas was
+          the most remote of the slave states, with a low presence of Union
+          troops, so enforcement of the proclamation had been slow and
+          inconsistent.
+        </span>
+      </div>
+
+      <br />
+
+      <div>
+        <span>
+          A common misconception is that this day marks the end of slavery in
+          the United States. Although this day marks the emancipation of all
+          slaves in the Confederacy, the institution of slavery was still legal
+          and existed in the Union border states after June 19, 1865. Slavery in
+          the United States did not officially end until the ratification of
+          the Thirteenth Amendment to the Constitution of the United States on
+          December 6, 1865, which abolished slavery entirely in all of the U.S.
+          states and territories.
+        </span>
+      </div>
+    </div>
+  );
+
+}
+
+export default Home;
